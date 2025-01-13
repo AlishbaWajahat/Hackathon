1eca5c1b5a6fdeb275766474df0e1b0cf57b1664
Alishba Wajahat <alishbawajahat867@gmail.com>
2025-01-12 22:14:26 -0800

    ..

--- app/singleproduct/[id]/page.tsx
+++ app/singleproduct/[id]/page.tsx
@@ -8,30 +8,30 @@ import React, { useState } from 'react'
 
 
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/book";
+// const API_URL =
+//   process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/book";
 export default async function Page({ params }: { params: { id: string } }) {
 
-    const fetching=await fetch(API_URL)
-    const data=await fetching.json()
-    const selectedProduct = data.find(
-        (product:product) => product.id === Number(params.id)
-    );
+    // const fetching=await fetch(API_URL)
+    // const data=await fetching.json()
+    // const selectedProduct = data.find(
+    //     (product:product) => product.id === Number(params.id)
+    // );
 
-    if (!selectedProduct) {
-        return <div>Product not found</div>; // Handle the case when no product is found
-    }
+    // if (!selectedProduct) {
+    //     return <div>Product not found</div>; // Handle the case when no product is found
+    // }
 
-    return (
-        <>
-            <Header />
-            <Singleproduct
-                ImageUrl={selectedProduct.imageUrl}
-                title={selectedProduct.title}
-                description={selectedProduct.description}
-                Cost={selectedProduct.cost}
-                pro={selectedProduct}
-            />
-        </>
-    );
+    // return (
+    //     <>
+    //         <Header />
+    //         <Singleproduct
+    //             ImageUrl={selectedProduct.imageUrl}
+    //             title={selectedProduct.title}
+    //             description={selectedProduct.description}
+    //             Cost={selectedProduct.cost}
+    //             pro={selectedProduct}
+    //         />
+    //     </>
+    // );
 }
