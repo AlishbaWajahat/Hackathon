b60f43c9c91a14e5ef75e05b1d72983aaec64da0
Alishba Wajahat <alishbawajahat867@gmail.com>
2025-01-25 18:57:23 -0800

    added a change

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -5,7 +5,7 @@ import { createClient } from 'next-sanity'
 import  dotenv  from 'dotenv';
 dotenv.config()
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
+  projectId: "drajzv77", 
   dataset: 'production', 
   apiVersion: '2021-08-31',
   useCdn: false,
