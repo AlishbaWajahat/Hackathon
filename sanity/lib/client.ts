390de1eeef69841c505d7618dfdeb2eb2c5f6ea1
Alishba Wajahat <alishbawajahat867@gmail.com>
2025-01-25 16:17:36 -0800

    fixed a deployment issue

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,10 +2,11 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId,token } from '../env'
 
+
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // Replace with your Sanity project ID
+  dataset: 'production', // Replace with your dataset name
+  apiVersion: '2021-08-31',
   useCdn: false,
-  token // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN, // Replace with your Sanity API token
+});
